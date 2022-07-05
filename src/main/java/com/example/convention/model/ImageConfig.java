cf5e7c9977d0c5d7dc2e52d97fb323926e350d0b
Toshiaki Maki <tmaki@vmware.com>
2022-07-06 01:36:58 +0900

    Use JsonNode instead of Map

--- src/main/java/com/example/convention/model/ImageConfig.java
+++ src/main/java/com/example/convention/model/ImageConfig.java
@@ -1,12 +1,13 @@
 package com.example.convention.model;
 
 import java.util.List;
-import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 /**
  * @param image a string reference to the image name and tag or associated digest.
  * @param boms an array of Bills of Materials (BOMs) describing the software components and their dependencies and may be zero or more per image.
  * @param config OCI image metadata
  */
-public record ImageConfig(String image, List<BOM> boms, Map<String, Object> config) {
+public record ImageConfig(String image, List<BOM> boms, JsonNode config) {
 }
